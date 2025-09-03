9c14e52987517b3a528477c407150d19fb76cbdf
alicewind <awind@byupathway.edu>
2025-09-03 12:38:59 -0600

    Back to original

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,15 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
-
-        Console.WriteLine();
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
-
+        Console.WriteLine("Hello World! This is the Exercise1 Project.");
     }
 }
