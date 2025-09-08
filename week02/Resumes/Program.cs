bae55971fff8083625a981490d3b22a4b391124f
alicewind <awind@byupathway.edu>
2025-09-08 11:41:57 -0600

    Back to Original

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,23 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
-        job1._company = "Microsoft";
-        job1._jobTitle = "Software Engineer";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
-
-        Job job2 = new Job();
-        job2._company = "Apple";
-        job2._jobTitle = "Senior Software Engineer";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
-
-        Resume resume = new Resume();
-        resume._jobs.Add(job1);
-        resume._jobs.Add(job2);
-        resume._name = "Allison Rose";
-
-        resume.Display();
+        Console.WriteLine("Hello World! This is the Resumes Project.");
     }
 }
