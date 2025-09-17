f7daaa09b4b0862af5cb5fd387b186bfd1f622f6
alicewind <awind@byupathway.edu>
2025-09-17 16:16:19 -0600

    Corrected naming style

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -5,7 +5,7 @@ public class Reference
     private string _book;
     private int _chapter;
     private int _verse;
-    private int _endverse;
+    private int _endVerse;
 
     public Reference(string book, int chapter, int verse)
     {
