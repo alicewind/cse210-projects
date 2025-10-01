79da1f6d85146c6560e523626053a2a03cd398e7
alicewind <awind@byupathway.edu>
2025-10-01 15:51:21 -0600

    Stubs created

---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,28 @@
+public class ReflectingActivity
+{
+    private List<string> _prompts = new List<string>();
+    private List<string> _questions = new List<string>();
+
+    public ReflectingActivity()
+
+    public void Run()
+    {
+
+    }
+    public string GetRandomPrompt()
+    {
+
+    }
+    public string GetRandomQuestion()
+    {
+
+    }
+    public void DisplayPrompt()
+    {
+
+    }
+    public void DisplayQuestions()
+    {
+        
+    }
+}
