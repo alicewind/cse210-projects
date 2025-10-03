7d202126c2db0b738d770b67e0d3cd2729a4f7de
alicewind <awind@byupathway.edu>
2025-10-03 14:17:35 -0600

    Reflecting Activity completed

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -15,11 +15,16 @@ class Program
             string menuChoice = Console.ReadLine();
             menuNumber = int.Parse(menuChoice);
 
-
             if (menuNumber == 1)
             {
                 BreathingActivity activity1 = new BreathingActivity("Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
                 activity1.Run();
+            }
+
+            else if (menuNumber == 2)
+            {
+                ReflectingActivity activity2 = new ReflectingActivity("Reflecting", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+                activity2.Run();
             }
         }
     }
--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -5,27 +5,74 @@ public class ReflectingActivity : Activity
 
     public ReflectingActivity(string name, string description) : base(name, description)
     {
+        _prompts.Add("Think of a time when you stood up for someone else.");
+        _prompts.Add("Think of a time when you did something really difficult.");
+        _prompts.Add("Think of a time when you helped someone in need.");
+        _prompts.Add("Think of a time when you did something truly selfless.");
 
+        _questions.Add("Why was this experience meaningful to you?");
+        _questions.Add("Have you ever done anything like this before?");
+        _questions.Add("How did you get started?");
+        _questions.Add("How did you feel when it was complete?");
+        _questions.Add("What made this time different than other times when you were not as successful?");
+        _questions.Add("What is your favorite thing about this experience?");
+        _questions.Add("What could you learn from this experience that applies to other situations?");
+        _questions.Add("What did you learn about yourself through this experience?");
+        _questions.Add("How can you keep this experience in mind in the future?");
     }
 
     public void Run()
     {
+        DisplayStartingMessage();
+        Console.Clear();
+        Console.WriteLine("Get ready... ");
+        ShowSpinner(6);
 
+        Console.WriteLine("Consider the following prompt:");
+        Console.WriteLine();
+        string prompt = GetRandomPrompt();
+        DisplayPrompt(prompt);
+        Console.WriteLine();
+
+        Console.WriteLine($"When you have something in mind, press Enter to continue.");
+        string enterText = Console.ReadLine();
+
+        Console.WriteLine("Now ponder on each of the following questions as they relate to this experience.");
+        Console.Write("You may begin in: ");
+        ShowCountDown(6);
+        Console.Clear();
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            string question = GetRandomQuestion();
+            DisplayQuestion(question);
+            Thread.Sleep(7000);
+        }
+
+        DisplayEndingMessage();
+        ShowSpinner(7);
     }
     public string GetRandomPrompt()
     {
-        return "";//fill in info
+        Random rand = new Random();
+        int r = rand.Next(_prompts.Count);
+        return _prompts[r];
     }
     public string GetRandomQuestion()
     {
-        return "";//fill in info
+        Random rand = new Random();
+        int r = rand.Next(_questions.Count);
+        return _questions[r];
     }
-    public void DisplayPrompt()
+    public void DisplayPrompt(string prompt)
     {
-
+        Console.WriteLine($"---- {prompt} ----");
     }
-    public void DisplayQuestions()
+    public void DisplayQuestion(string question)
     {
-
+        Console.WriteLine($"{question}");
     }
 }
