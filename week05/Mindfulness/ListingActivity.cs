bf601e473f1ecf3d4bc16b3d563fe4fb03a4a911
alicewind <awind@byupathway.edu>
2025-10-03 20:12:25 -0600

    Listing Activity complete

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 public class ListingActivity : Activity
 {
     private int _count;
@@ -5,20 +7,53 @@ public class ListingActivity : Activity
 
     public ListingActivity(string name, string description) : base(name, description)
     {
-
+        _prompts.Add("Who are people that you appreciate?");
+        _prompts.Add("What are personal strengths of yours?");
+        _prompts.Add("Who are people that you have helped this week?");
+        _prompts.Add("When have you felt the Holy Ghost this month?");
+        _prompts.Add("Who are some of your personal heroes?");
     }
 
     public void Run()
     {
+        DisplayStartingMessage();
+        Console.Clear();
+        Console.WriteLine("Get ready... ");
+        ShowSpinner(6);
+        Console.WriteLine();
 
+        Console.WriteLine("List as many responses you can to the following prompt: ");
+        GetRandomPrompt();
+        Console.Write("You may begin in: ");
+        ShowCountDown(7);
+        Console.WriteLine();
+
+        GetListFromUser();
+        Console.WriteLine($"You listed {_count} items!");
+
+        DisplayEndingMessage();
+        ShowSpinner(7);
     }
     public void GetRandomPrompt()
     {
-
+        Random rand = new Random();
+        int r = rand.Next(_prompts.Count);
+        Console.WriteLine($"---- {_prompts[r]} ----");
     }
     public List<string> GetListFromUser()
     {
+        List<string> responses = new List<string>();
 
-        return _prompts;
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            responses.Add(Console.ReadLine());
+        }
+        int count = responses.Count();
+        _count = count;
+        return responses;
     }
 }
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -26,6 +26,12 @@ class Program
                 ReflectingActivity activity2 = new ReflectingActivity("Reflecting", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
                 activity2.Run();
             }
+
+            else if (menuNumber == 3)
+            {
+                ListingActivity activity3 = new ListingActivity("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+                activity3.Run();
+            }
         }
     }
 }
