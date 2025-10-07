1686fa4d1d7beca47e9ba50810d0d84e98c78c98
alicewind <awind@byupathway.edu>
2025-10-07 16:20:19 -0600

    Wk 6 Shapes Homework Complete

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("blue", 5);
+        //Console.WriteLine($"{square1.GetColor()}");
+        //Console.WriteLine($"{square1.GetArea()}");
+
+        Rectangle rectangle1 = new Rectangle("red", 7, 4);
+        //Console.WriteLine($"{rectangle1.GetColor()}");
+        //Console.WriteLine($"{rectangle1.GetArea()}");
+
+        Circle circle1 = new Circle("yellow", 8);
+        //Console.WriteLine($"{circle1.GetColor()}");
+        //Console.WriteLine($"{circle1.GetArea()}");
+
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
